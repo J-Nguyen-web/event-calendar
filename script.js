509b65f941f0aa41c63f997b1e7689a725596cda
J-Nguyen-web <map.genshin.impacts@gmail.com>
2025-10-03 23:26:36 +0300

    build calendar added

--- script.js
+++ script.js
@@ -3,4 +3,16 @@ let events = JSON.parse(localStorage.getItem('timelineEvents') || '[]');
 
 let startDate = new Date("2025-05-01");
 let endDate = new Date("2025-08-01");
-let dayWIdth = 90;  
+let dayWIdth = 90;
+
+function buildCalendar() {
+    wrapper.innerHTML='';
+    let date = new Date(startDate);
+    while(date <= endDate){
+        let month = date.getMonth();
+        let year = date.getFullYear();
+
+        let row = document.createElement('div');
+        row.className = 'month-row';
+    }
+}
